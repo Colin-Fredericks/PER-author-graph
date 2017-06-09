67972806ddc560c3abbc51496b92078d1f432e22
Colin-Fredericks <colin.fredericks@gmail.com>
2017-06-09 16:16:09 -0400

    more size/color

--- multitool/d3v4-selectable-force-directed-graph_v2.js
+++ multitool/d3v4-selectable-force-directed-graph_v2.js
@@ -87,7 +87,7 @@ function createV4SelectableForceDirectedGraph(svg, graph) {
     var nodeText = nodeGroup.append("text")
         .text(function(d){ return d.initials; })
         .attr("text-size", function(d){
-          return Math.min(Math.sqrt(d.publications) * 2, 10);
+          return Math.max(Math.sqrt(d.publications) * 2, 10);
         })
         .attr("fill","black")
         .call(d3v4.drag()

6896cdeff44afe682ddadacb4dace4bc348d597a
Colin-Fredericks <colin.fredericks@gmail.com>
2017-06-11 12:29:28 -0400

    logging time

--- multitool/d3v4-selectable-force-directed-graph_v3.js
+++ multitool/d3v4-selectable-force-directed-graph_v3.js
@@ -257,6 +257,7 @@ function createV4SelectableForceDirectedGraph(svg, graph) {
     }
     
     function addNameToList(d){
+        console.log(d);
         var authorList = d3v4.select('#namelist');
         authorList.selectAll('p').remove();
         if('name' in d){

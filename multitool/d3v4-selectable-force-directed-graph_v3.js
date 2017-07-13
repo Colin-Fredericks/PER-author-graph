161546b47179b8d371f2cd82fdc58018bfbe4ca0
Colin-Fredericks <colin.fredericks@gmail.com>
2017-07-12 20:51:54 -0400

    Ugh, ok, forget it. I'll do it later.

--- multitool/d3v4-selectable-force-directed-graph_v3.js
+++ multitool/d3v4-selectable-force-directed-graph_v3.js
@@ -152,14 +152,8 @@ function createV4SelectableForceDirectedGraph(svg, graph) {
     // Add author names to list
     var authors = authorList
         .selectAll('p')
-        .data(graph.nodes);
-    
-    authors.sort(function(d){
-        	return d3.ascending(d.name);
-    });
-
-    
-    authors.enter().append('p')
+        .data(graph.nodes)
+        .enter().append('p')
         .classed('authorname', true)
         .text(function(d){ return d.name })
         .on('click', function(d){ updateInfo(d) });

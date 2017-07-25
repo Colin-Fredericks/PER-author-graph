749ffe8cd96690d389d3aae567575c204ea6fd38
Colin-Fredericks <colin.fredericks@gmail.com>
2017-07-25 10:44:13 -0400

    and tweaking

--- multitool/d3v4-selectable-force-directed-graph_v4.js
+++ multitool/d3v4-selectable-force-directed-graph_v4.js
@@ -353,9 +353,9 @@ function createV4SelectableForceDirectedGraph(svg, graph) {
         
         // Moving the nodes causes strange and inexplicable behavior.
         // I need to move the viewport.
-        d3.selectAll('circle').attr('transform', 'translate('+(xOffset + width / 2)+','+(yOffset + height/2)+')');
-        d3.selectAll('line').attr('transform', 'translate('+(xOffset + width / 2)+','+(yOffset + height/2)+')');
-        d3.selectAll('text').attr('transform', 'translate('+(xOffset + width / 2)+','+(yOffset + height/2)+')');
+        d3.selectAll('circle').attr('transform', 'translate('+(xOffset + width)+','+(yOffset + height)+')');
+        d3.selectAll('line').attr('transform', 'translate('+(xOffset + width)+','+(yOffset + height)+')');
+        d3.selectAll('text').attr('transform', 'translate('+(xOffset + width)+','+(yOffset + height)+')');
         
         //Another maybe:
         //simulation.force('center', d3.forceCenter(parentWidth / 2 + xOffset, parentHeight / 2 + yOffset))

bdac26a9889241f47900c84da2ba71110f7b686c
Colin-Fredericks <colin.fredericks@gmail.com>
2017-07-25 11:00:00 -0400

    Ok, let's try the alternate version.

--- multitool/d3v4-selectable-force-directed-graph_v4.js
+++ multitool/d3v4-selectable-force-directed-graph_v4.js
@@ -354,14 +354,13 @@ function createV4SelectableForceDirectedGraph(svg, graph) {
         // Moving the nodes causes strange and inexplicable behavior.
         // I need to move the viewport.
 
-// I can't seem to move the author to the actual center.
-        console.log('delta x: '+(-Number(author.x)+width/2)+', delta y: '+(-Number(author.y+height/2)))
-        d3.selectAll('circle').attr('transform', 'translate('+(-Number(author.x)+width/2)+','+(-Number(author.y+height/2))+')');
-        d3.selectAll('line').attr('transform', 'translate('+(-Number(author.x)+width/2)+','+(-Number(author.y)+height/2)+')');
-        d3.selectAll('text').attr('transform', 'translate('+(-Number(author.x)+width/2)+','+(-Number(author.y)+height/2)+')');
+        // I can't seem to move the author to the actual center. I have no idea why changing these numbers doesn't do much.
+//         d3.selectAll('circle').attr('transform', 'translate('+(-Number(author.x)+width/2)+','+(-Number(author.y+height/2))+')');
+//         d3.selectAll('line').attr('transform', 'translate('+(-Number(author.x)+width/2)+','+(-Number(author.y)+height/2)+')');
+//         d3.selectAll('text').attr('transform', 'translate('+(-Number(author.x)+width/2)+','+(-Number(author.y)+height/2)+')');
         
         //Another maybe:
-        //simulation.force('center', d3.forceCenter(parentWidth / 2 + xOffset, parentHeight / 2 + yOffset))
+        simulation.force('center', d3.forceCenter(author.x, author.y))
         
     }
     

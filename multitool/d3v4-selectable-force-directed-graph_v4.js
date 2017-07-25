fa320f232d88159a321bb52310dd9ee848d76d2d
Colin-Fredericks <colin.fredericks@gmail.com>
2017-07-25 11:45:52 -0400

    Checking new location

--- multitool/d3v4-selectable-force-directed-graph_v4.js
+++ multitool/d3v4-selectable-force-directed-graph_v4.js
@@ -364,6 +364,7 @@ function createV4SelectableForceDirectedGraph(svg, graph) {
         
         //Another maybe:
        simulation.force('center', d3.forceCenter(2*xOffset + author.x, 2*yOffset + author.y));
+       console.log('new location: x=' + author.x + ', y=' + author.y);
        simulation.restart();
         
     }

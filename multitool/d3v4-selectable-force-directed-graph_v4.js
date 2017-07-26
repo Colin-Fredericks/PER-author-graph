b84513f336d962e5400f7a14b897b3f406f4e1f4
Colin-Fredericks <colin.fredericks@gmail.com>
2017-07-26 10:21:08 -0400

    It didn't! What if I changed fixed position?

--- multitool/d3v4-selectable-force-directed-graph_v4.js
+++ multitool/d3v4-selectable-force-directed-graph_v4.js
@@ -355,10 +355,10 @@ function createV4SelectableForceDirectedGraph(svg, graph) {
         
         // Another possibility: move force center, update positions.
         simulation.force('center', d3.forceCenter(2*xOffset + author.x, 2*yOffset + author.y));
-//         node.each(function(d){
-//             d.x += xOffset;
-//             d.y += yOffset;
-//         });
+        node.each(function(d){
+            d.fx += xOffset;
+            d.fy += yOffset;
+        });
         console.log('new location: x=' + author.x + ', y=' + author.y);
         simulation.restart();
         
